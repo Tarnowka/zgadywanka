baa0214dab32f162c3ec1acb8b91572b752b667c
Tarnowka <48628472+Tarnowka@users.noreply.github.com>
2019-05-18 18:25:31 +0200

    test

--- GraZaDużoZaMało/ModelGry/Gra.cs
+++ GraZaDużoZaMało/ModelGry/Gra.cs
@@ -4,6 +4,20 @@ namespace ModelGry
 {
     public class Gra
     {
+        //pola
+        int wylosowana;
+        int zakresOD, zakresDO;
 
+        //ToDo: historia rozgrywki
+        public Gra(int min, int max)
+        {
+            wylosowana = Losuj(min, max);
+        }
+        private int Losuj(int min = 1, int max = 100)
+        {
+            Random generator = new Random();
+            int wylosowana = generator.Next(min, max + 1);
+            return wylosowana;
+        }
     }
 }
