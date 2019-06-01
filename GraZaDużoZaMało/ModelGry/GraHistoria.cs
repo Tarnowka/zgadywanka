6c90cf522af01a12e409a98eb16dff5aee9b4c52
Tarnowka <48628472+Tarnowka@users.noreply.github.com>
2019-06-01 19:30:38 +0200

    gramodel

--- GraZaDużoZaMało/ModelGry/GraHistoria.cs
+++ GraZaDużoZaMało/ModelGry/GraHistoria.cs
@@ -6,5 +6,25 @@ namespace ModelGry
 {
     public partial class Gra
     {
+        // historia gry
+        public List<Ruch> Historia;
+
+        // inner class
+        public class Ruch
+        {
+            public readonly int Propozycja;
+            public readonly Odp Odpowiedz;
+            public readonly DateTime Kiedy;
+
+            public Ruch(int prop, Odp odp)
+            {
+                Propozycja = prop;
+                Odpowiedz = odp;
+                Kiedy = DateTime.Now;
+            }
+
+            public override string ToString()
+                => $"({Propozycja}, {Odpowiedz}, {Kiedy})";
+        }   // koniec wewnętrzenej klasy Ruch
     }
 }
